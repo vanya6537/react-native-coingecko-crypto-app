774e1df8f775b181b6a528299c9eac54c16798d6
Ivan Katkov <vanya6537@gmail.com>
2026-04-09 22:42:05 +0700

    feat: initialize native iOS and Android platform folders

---
+++ android/app/src/main/java/com/cryptotokensapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.cryptotokensapp;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+
+  @Override
+  protected String getMainComponentName() {
+    return "CryptoTokensApp";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+}
