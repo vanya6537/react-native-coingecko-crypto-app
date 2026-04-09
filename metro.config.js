0d76edf2e8aca9734c2876826787b84fda3647ca
Ivan Katkov <vanya6537@gmail.com>
2026-04-09 20:52:34 +0700

    fix: add maxWorkers to metro.config for Node 20 compatibility

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,7 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const config = {
   projectRoot: __dirname,
   watchFolders: [__dirname],
+  maxWorkers: 2,
 };
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
