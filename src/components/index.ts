72c9e8619096df288205aa9448cb8fe6de56ddea
Ivan Katkov <vanya6537@gmail.com>
2026-04-10 19:08:49 +0700

    feat: Export FilterBar component in index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { TokenItem } from './TokenItem';
 export { TokenList } from './TokenList';
+export { FilterBar } from './FilterBar';
 export { PriceChart } from './PriceChart';
 export { ExpandedPriceChart } from './ExpandedPriceChart';
 export { SkeletonLoader, TokenItemSkeleton, TokenListLoadingSkeleton } from './SkeletonLoader';
