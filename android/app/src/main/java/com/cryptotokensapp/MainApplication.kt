bb974ce17e4ac799b5b4cef60e152763aeab8cf8
Ivan Katkov <vanya6537@gmail.com>
2026-04-11 04:58:26 +0700

    build: remove MainActivity and MainApplication files, update MainApplication.kt for new architecture and disable Hermes

--- android/app/src/main/java/com/cryptotokensapp/MainApplication.kt
+++ android/app/src/main/java/com/cryptotokensapp/MainApplication.kt
@@ -1,22 +1,24 @@
 package com.cryptotokensapp
 
 import android.app.Application
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
-import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
-import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
-  override val reactHost: ReactHost by lazy {
-    getDefaultReactHost(
-      context = applicationContext,
-      packageList = PackageList(this).packages
-    )
+  private val mReactNativeHost = object : DefaultReactNativeHost(this) {
+    override fun getUseDeveloperSupport(): Boolean = true
+
+    override fun getPackages(): MutableList<ReactPackage> = mutableListOf()
   }
+
+  override val reactNativeHost: ReactNativeHost
+    get() = mReactNativeHost
 
   override fun onCreate() {
     super.onCreate()
-    loadReactNative(this)
+    SoLoader.init(this, false)
   }
 }
