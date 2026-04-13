c34a8147a3e899fcc07d66506a328a08799c6116
Ivan Katkov <vanya6537@gmail.com>
2026-04-13 07:14:17 +0700

    refactor: remove unused screen components and update index export

--- src/screens/index.ts
+++
@@ -1,4 +0,0 @@
-export { LoginScreen } from './LoginScreen';
-export { TokensListScreen } from './TokensListScreen';
-export { TokenDetailScreen } from './TokenDetailScreen';
-export { PriceChartScreen } from './PriceChartScreen';
